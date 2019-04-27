0329e415ae082364470ea166afc7510256b99c7d
Anthony Chang <anthony.chang@utexas.edu>
2019-04-27 14:45:15 -0500

    Added seperate CSS sheet Added button that links to FEMA website

--- popup.js
+++ popup.js
@@ -8,12 +8,12 @@
   navigator.geolocation.getCurrentPosition(geoSuccess);
 
 };*/
-
+//gets current location of user
 function findMe() {
 	let status = document.getElementById('status');
 
 	function success(position) {
-		let lat = position.coords.latitiude;
+		let lat = position.coords.latitude;
 		let lon = position.coords.longitude;
 		document.getElementById('startLat').innerHTML = lat;
   		document.getElementById('startLon').innerHTML = lon;
@@ -23,12 +23,24 @@ function findMe() {
 	function error() {
 		status.innerHTML = 'Unable to retrieve location';
 	}
+	status.innerHTML = 'Locating...';
 	navigator.geolocation.getCurrentPosition(success, error);
 }
 
+function test() {
+	let status = document.getElementById('status');
+	status.innerHTML = 'works';
+}
+
+//button to get location of user
 let button = document.getElementById('find-me')
 if (button) {
 	button.addEventListener('click', findMe);
-}
+} else
+	status = 'Error with button';
 
+let floodButton = document.getElementById('flood-map')
+if (floodButton) {
+	floodButton.addEventListener('click', test);
+} 
 
